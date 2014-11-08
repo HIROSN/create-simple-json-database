0de0d2090d6d6445e158bb6900af719c7eac8a59
Hiroshi Nagata <hirosn@live.com>
2014-11-07 17:39:15 -0800

    Turning in create a simple JSON database assignment.

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
 'use strict';
+
+var express = require('express');
+var bodyparser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+var dir = __dirname + '/data/';
+var ext = '.json';
+
+app.use(bodyparser.json());
+app.set('port', process.env.PORT || 3000);
+
+app.post('/:filename', function(req, res) {
+  var str = JSON.stringify(req.body);
+  var path = dir + req.params.filename + ext;
+
+  fs.mkdir(dir, '0777', function(err) {
+    if (err && err.code !== 'EEXIST') { return res.status(500).end(); }
+
+    fs.writeFile(path, str, function(err) {
+      if (err) { return res.status(500).end(); }
+      res.end();
+    });
+  });
+});
+
+app.get('/:filename', function(req, res) {
+  var path = dir + req.params.filename + ext;
+
+  fs.readFile(path, {encoding: 'utf8'}, function(err, data) {
+    if (err) { return res.status(500).end(); }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(app.get('port'));
---
+++ test/api_test.js
@@ -0,0 +1,141 @@
+'use strict';
+
+var chai = require('chai');
+var chaihttp = require('chai-http');
+var fs = require('fs');
+
+var server = 'http://localhost:' + (process.env.PORT || 3000);
+var expect = chai.expect;
+var dir = __dirname + '/../data';
+
+require('../server.js');
+chai.use(chaihttp);
+
+describe('Simple JSON database', function() {
+  it('should post JSON data to file1', function(done) {
+    chai.request(server).
+      post('/file1').
+      send({message: 'hello world'}).
+      end(function(err, res) {
+        expect(err).equals(null);
+        expect(res).to.be.a('object');
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('should make another post to file2', function(done) {
+    chai.request(server).
+      post('/file2').
+      send({msg: 'foo', code: 47}).
+      end(function(err, res) {
+        expect(err).equals(null);
+        expect(res).to.be.a('object');
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('should get JSON data from file1', function(done) {
+    chai.request(server).
+      get('/file1').
+      end(function(err, res) {
+        expect(err).equals(null);
+        expect(res).to.be.a('object');
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.property('message');
+        expect(res.body.message).to.be.a('string');
+        expect(res.body.message).equals('hello world');
+        done();
+      });
+  });
+
+  it('should get JSON data from file2', function(done) {
+    chai.request(server).
+      get('/file2').
+      end(function(err, res) {
+        expect(err).equals(null);
+        expect(res).to.be.a('object');
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.property('msg');
+        expect(res.body.msg).to.be.a('string');
+        expect(res.body.msg).equals('foo');
+        expect(res.body).to.have.property('code');
+        expect(res.body.code).to.be.a('number');
+        expect(res.body.code).equals(47);
+        done();
+      });
+  });
+
+  it('should post JSON data to existing file1', function(done) {
+    chai.request(server).
+      post('/file1').
+      send({message: 'foo bar'}).
+      end(function(err, res) {
+        expect(err).equals(null);
+        expect(res).to.be.a('object');
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('should get JSON data from updated file1', function(done) {
+    chai.request(server).
+      get('/file1').
+      end(function(err, res) {
+        expect(err).equals(null);
+        expect(res).to.be.a('object');
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.property('message');
+        expect(res.body.message).to.be.a('string');
+        expect(res.body.message).equals('foo bar');
+        done();
+      });
+  });
+
+  it('should return error when failed to save the post', function(done) {
+    fs.unlink(dir + '/file1.json', function(err) {
+      expect(err).equals(null);
+
+      fs.unlink(dir + '/file2.json', function(err) {
+        expect(err).equals(null);
+
+        fs.rmdir(dir, function(err) {
+          expect(err).equals(null);
+
+          fs.writeFile(dir, 'foo', function(err) {
+            expect(err).equals(null);
+
+            chai.request(server).
+              post('/file1').
+              send({message: 'hello world'}).
+              end(function(err, res) {
+                expect(err).equals(null);
+                expect(res).to.be.a('object');
+                expect(res).to.have.status(500);
+                done();
+              });
+          });
+        });
+      });
+    });
+  });
+
+  it('should return error when failed to find the file', function(done) {
+    fs.unlink(dir, function(err) {
+      expect(err).equals(null);
+
+      chai.request(server).
+        get('/file1').
+        end(function(err, res) {
+          expect(err).equals(null);
+          expect(res).to.be.a('object');
+          expect(res).to.have.status(500);
+          done();
+        });
+    });
+  });
+});
